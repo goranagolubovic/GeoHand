de6d4074f581735462aca4a3f192c7d9f2a27a49
goranagolubovic <45577207+goranagolubovic@users.noreply.github.com>
2023-03-20 21:10:47 +0100

    landmarks logic implemented

---
+++ constants/landmarks-api.js
@@ -0,0 +1,15 @@
+export const CLIENT_ID = "YSKRDTUMHEZ5NOW5CHVH5COSG0IBDSNEM35CIRD2JTSM4KT1";
+export const CLIENT_SECRET = "RWSMI2LRUPDG3EO5OREUFDDL3F3RXM5DZM5XFXCU1ZC0TT2P";
+export const GOOGLE_MAPS_API_KEY = AIzaSyC8IW6TpXZV_n6AeZCevMNUC - eDLfVaZZg;
+// export const FOURSQUARE_API_URL =
+//   "https://api.foursquare.com/v3/venues/explore";
+//   "https://api.foursquare.com/v2/venues/explore/?categoryId=4bf58dd8d48988d12d941735&limit=10&client_id=" +
+//   CLIENT_ID +
+//   "&client_secret=" +
+//   CLIENT_SECRET +
+//   "&v=";
+// https://api.foursquare.com/v2/venues/explore/?categoryId=4bf58dd8d48988d12d941735&limit=10&client_id=YSKRDTUMHEZ5NOW5CHVH5COSG0IBDSNEM35CIRD2JTSM4KT1&client_secret=RWSMI2LRUPDG3EO5OREUFDDL3F3RXM5DZM5XFXCU1ZC0TT2P&v=20220316&near=France
+export const GOOGLE_MAPS_API =
+  "https://maps.googleapis.com/maps/api/place/textsearch/json?key=" +
+  GOOGLE_MAPS_API_KEY +
+  "&query=landmark+in+France&type=tourist_attraction&radius=50000";
